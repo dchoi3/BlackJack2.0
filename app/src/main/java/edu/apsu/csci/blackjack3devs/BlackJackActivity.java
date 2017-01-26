b0187bd0db17138bf4d767ebe17243786e8f6962
Daniel <dchoi3@my.aspu.edu>
2017-01-25 23:16:20 -0600

    Created Start Button. Created an Intent for the second activity. Temp Background color.
    Feel free to change whatever, but I thought it be nice to have a home menu.

---
+++ app/src/main/java/edu/apsu/csci/blackjack3devs/BlackJackActivity.java
@@ -0,0 +1,13 @@
+package edu.apsu.csci.blackjack3devs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BlackJackActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_black_jack);
+    }
+}
--- app/src/main/java/edu/apsu/csci/blackjack3devs/MainActivity.java
+++ app/src/main/java/edu/apsu/csci/blackjack3devs/MainActivity.java
@@ -1,7 +1,10 @@
 package edu.apsu.csci.blackjack3devs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton start = (ImageButton) findViewById(R.id.startButton);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(getApplicationContext(), BlackJackActivity.class);
+                startActivity(startIntent);
+            }
+        });
+
+
     }
 }
