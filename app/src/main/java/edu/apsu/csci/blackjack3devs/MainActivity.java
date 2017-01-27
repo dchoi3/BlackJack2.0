272d1e73ec7ac7cc8f514ff1fde046d3ad30265c
Daniel <dchoi3@my.apsu.edu>
2017-01-27 14:56:13 -0600

    Catching up commit. Finally got it to work

--- app/src/main/java/edu/apsu/csci/blackjack3devs/MainActivity.java
+++ app/src/main/java/edu/apsu/csci/blackjack3devs/MainActivity.java
@@ -1,10 +1,7 @@
 package edu.apsu.csci.blackjack3devs;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,16 +9,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        ImageButton start = (ImageButton) findViewById(R.id.startButton);
-
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent startIntent = new Intent(getApplicationContext(), BlackJackActivity.class);
-                startActivity(startIntent);
-            }
-        });
+        //Testing github
 
 
     }
