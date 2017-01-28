d7cc644557991e4a0e0d2789ae770332ef5e5df1
Daniel <dchoi3@my.apsu.edu>
2017-01-27 20:35:47 -0600

    created about intent.

--- app/src/main/java/edu/apsu/csci/blackjack3devs/MainActivity.java
+++ app/src/main/java/edu/apsu/csci/blackjack3devs/MainActivity.java
@@ -9,8 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Testing github
-
-
+        
     }
 }
