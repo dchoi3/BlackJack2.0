38f87beed63762819e50e2de9efe60766b7fdf57
jschmitt4 <jschmitt4@my.apsu.edu>
2017-01-31 23:08:50 -0500

    Schmitt Merge (#3)
    * adds lots of code to display all playable cards so I can get a better idea of what is happening.
    
    * deal method is dealing initial two cards randomly
    
    * Screen locked to portrait

--- app/src/main/java/edu/apsu/csci/blackjack3devs/BlackJackActivity.java
+++ app/src/main/java/edu/apsu/csci/blackjack3devs/BlackJackActivity.java
@@ -3,6 +3,7 @@ package edu.apsu.csci.blackjack3devs;
 import android.support.annotation.DrawableRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -11,13 +12,43 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Random;
+
 import static android.R.attr.id;
+import static android.R.attr.logoDescription;
 
 public class BlackJackActivity extends AppCompatActivity
         implements View.OnClickListener{
     // Variable to hold bet amount
     int CurrentBetAmt = 0;
     int walletAmt = 2000;
+
+    // Initialize Card drawable/value array.
+    private int[][] cardsArray = {
+        {R.drawable.c2,2},{R.drawable.h2,2},{R.drawable.d2,2},{R.drawable.s2,2},
+        {R.drawable.c3,3},{R.drawable.h3,3},{R.drawable.d3,3},{R.drawable.s3,3},
+        {R.drawable.c4,4},{R.drawable.h4,4},{R.drawable.d4,4},{R.drawable.s4,4},
+        {R.drawable.c5,5},{R.drawable.h5,5},{R.drawable.d5,5},{R.drawable.s5,5},
+        {R.drawable.c6,6},{R.drawable.h6,6},{R.drawable.d6,6},{R.drawable.s6,6},
+        {R.drawable.c7,7},{R.drawable.h7,7},{R.drawable.d7,7},{R.drawable.s7,7},
+        {R.drawable.c8,8},{R.drawable.h8,8},{R.drawable.d8,8},{R.drawable.s8,8},
+        {R.drawable.c9,9},{R.drawable.h9,9},{R.drawable.d9,9},{R.drawable.s9,9},
+        {R.drawable.c10,10},{R.drawable.h10,10},{R.drawable.d10,10},{R.drawable.s10,10},
+        {R.drawable.cj,10},{R.drawable.hj,10},{R.drawable.dj,10},{R.drawable.sj,10},
+        {R.drawable.cq,10},{R.drawable.hq,10},{R.drawable.dq,10},{R.drawable.sq,10},
+        {R.drawable.ck,10},{R.drawable.hk,10},{R.drawable.dk,10},{R.drawable.sk,10},
+        {R.drawable.ca,11},{R.drawable.ha,11},{R.drawable.da,11},{R.drawable.sa,11},
+    };
+
+    // Put card view ids in array.
+    private int[] houseCardsID = {
+        R.id.houseC1,R.id.houseC2,R.id.houseC3,R.id.houseC4,R.id.houseC5,
+        R.id.houseC6,R.id.houseC7,R.id.houseC8,R.id.houseC9,
+    };
+    private int[] playerCardsID = {
+        R.id.playerC1,R.id.playerC2,R.id.playerC3,R.id.playerC4,R.id.playerC5,
+        R.id.playerC6,R.id.playerC7,R.id.playerC8,R.id.playerC9,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,7 +59,7 @@ public class BlackJackActivity extends AppCompatActivity
             R.id.hitButton, R.id.standButton, R.id.doubleButton,R.id.dealButton};
 
         clearBoard();
-        shuffleCards();
+        // shuffleCards(); I don't think we need this here.
 
         for(int id : buttonsID){
             ImageButton ib = (ImageButton) findViewById(id);
@@ -115,7 +146,9 @@ public class BlackJackActivity extends AppCompatActivity
 
     public void clearBoard(){
 
-        int [] cardsID = {R.id.houseC1,R.id.houseC2,R.id.houseC3,R.id.houseC4,R.id.houseC5,R.id.houseC6,R.id.houseC7,R.id.houseC8,R.id.houseC9,R.id.playerC1,R.id.playerC2,R.id.playerC3,R.id.playerC4,R.id.playerC5,R.id.playerC6,R.id.playerC7,R.id.playerC8,R.id.playerC9,};
+        int [] cardsID = {R.id.houseC1,R.id.houseC2,R.id.houseC3,R.id.houseC4,R.id.houseC5,R.id.houseC6,
+            R.id.houseC7,R.id.houseC8,R.id.houseC9,R.id.playerC1,R.id.playerC2,R.id.playerC3,
+            R.id.playerC4,R.id.playerC5,R.id.playerC6,R.id.playerC7,R.id.playerC8,R.id.playerC9,};
         for(int id : cardsID){
             ImageView iv = (ImageView) findViewById(id);
             iv.setVisibility(View.INVISIBLE);
@@ -135,44 +168,51 @@ public class BlackJackActivity extends AppCompatActivity
         tv.setText("$0");
     }
 
-    public void shuffleCards(){
-
+    /**
+     * shuffleCards uses multidimensional array to set a
+     * randomly selected card's image and value.
+     * @return Returns the index of a single random card.
+     */
+    public int shuffleCards() {
+        Random randomCardGenerator = new Random();
+        // We know there are 52 cards, so 51 is hardcoded for the index of 0-51.
+        return randomCardGenerator.nextInt(51);
     }
+
     public void deal(){
         //Check to make sure there is a bet value before executing hitShow & standShow
         if(CurrentBetAmt > 0) {
-            ImageButton dealClear = (ImageButton) findViewById(R.id.dealButton);
-            dealClear.setVisibility(View.INVISIBLE);
-            //Can set betTextView to say "Place bet" or something.
-            ImageButton hitShow = (ImageButton) findViewById(R.id.hitButton);
-            hitShow.setVisibility(View.VISIBLE);
+        ImageButton dealClear = (ImageButton) findViewById(R.id.dealButton);
+        dealClear.setVisibility(View.INVISIBLE);
+        //Can set betTextView to say "Place bet" or something.
+        ImageButton hitShow = (ImageButton) findViewById(R.id.hitButton);
+        hitShow.setVisibility(View.VISIBLE);
 
-            ImageButton standShow = (ImageButton) findViewById(R.id.standButton);
-            standShow.setVisibility(View.VISIBLE);
+        ImageButton standShow = (ImageButton) findViewById(R.id.standButton);
+        standShow.setVisibility(View.VISIBLE);
 
-            //This would have to be different to show shuffled cards
-            ImageView hC1 = (ImageView) findViewById(R.id.houseC1);
-            hC1.setVisibility(View.VISIBLE);
-            hC1.setImageResource(R.drawable.facedown);
+        // Place two cards for player and dealer.
+        for (int i = 0; i < 2; i++) {
 
-            ImageView hC2 = (ImageView) findViewById(R.id.houseC2);
-            hC2.setVisibility(View.VISIBLE);
-            hC2.setImageResource(R.drawable.sa);
+            // Player card.
+            int playerCardIndex = shuffleCards();
+            int playerCardValue = cardsArray[playerCardIndex][1];
+            ImageView playerCard = (ImageView) findViewById(playerCardsID[i]);
+            playerCard.setVisibility(View.VISIBLE);
+            playerCard.setImageResource(cardsArray[playerCardIndex][0]);
 
-            ImageView pC1 = (ImageView) findViewById(R.id.playerC1);
-            pC1.setVisibility(View.VISIBLE);
-            pC1.setImageResource(R.drawable.c5);
-
-            ImageView pC2 = (ImageView) findViewById(R.id.playerC2);
-            pC2.setVisibility(View.VISIBLE);
-            pC2.setImageResource(R.drawable.h10);
-
-            ImageView pC3 = (ImageView) findViewById(R.id.playerC3);
-            pC3.setVisibility(View.VISIBLE);
-            pC3.setImageResource(R.drawable.h8);
-
-            updateCardTotal();
+            // House card.
+            int houseCardIndex = shuffleCards();
+            int houseCardValue = cardsArray[houseCardIndex][1];
+            ImageView houseCard = (ImageView) findViewById(houseCardsID[i]);
+            houseCard.setVisibility(View.VISIBLE);
+            if (i == 0) {
+                houseCard.setImageResource(R.drawable.facedown);
+            } else {
+                houseCard.setImageResource(cardsArray[houseCardIndex][0]);
+            }
         }
+        updateCardTotal();
     }
     public void hit(){
 
